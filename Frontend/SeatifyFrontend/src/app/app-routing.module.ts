b4cab06cb1e9a99f3a66100cc13ca6dda9149d8c
batoriandras <batori.andras@gmail.com>
2026-03-10 23:49:05 +0100

    routing test

--- Frontend/SeatifyFrontend/src/app/app-routing.module.ts
+++ Frontend/SeatifyFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './Components/WelcomePage/welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: 'welcome', component: WelcomePageComponent},
+  {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
