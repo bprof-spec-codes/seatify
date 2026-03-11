1bd0754e3d357868d30b1d579794758ab9b50389
batoriandras <batori.andras@gmail.com>
2026-03-11 13:32:09 +0100

    Entityframeworkcore nuget packages, Repository, AppDbContext created

---
+++ Backend/SeatifyBackend/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> ctx) : base(ctx)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
