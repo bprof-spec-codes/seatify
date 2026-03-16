356ffa06f5a8273c4cea7a9f0a692adcdc296bbb
batoriandras <batori.andras@gmail.com>
2026-03-16 16:30:47 +0100

    Add AuditoriumCreateDto and update Venue with Auditoriums

--- Backend/SeatifyBackend/Entities/Models/Venue.cs
+++ Backend/SeatifyBackend/Entities/Models/Venue.cs
@@ -2,9 +2,9 @@
 {
     public class Venue
     {
-        public string Id { get; set; }
-        string.Empty;
+        public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
+        public ICollection<Auditorium> Auditoriums { get; set; } = new List<Auditorium>();
         public Venue()
         {
             Id = Guid.NewGuid().ToString();
