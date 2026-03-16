d24b3e1a570b2ce6e890e842db6e261fc5d16ae3
batoriandras <batori.andras@gmail.com>
2026-03-16 16:25:09 +0100

    Add Venue entity and link it to Auditorium

--- Backend/SeatifyBackend/Entities/Models/Auditorium.cs
+++ Backend/SeatifyBackend/Entities/Models/Auditorium.cs
@@ -6,6 +6,7 @@ namespace Entities.Models
     {
         public string Id { get; set; } = string.Empty;
         public string VenueId { get; set; } = string.Empty;
+        public Venue Venue { get; set; } = null!;
         [StringLength(50)]
         public string Name { get; set; } = string.Empty;
         [StringLength(500)]
---
+++ Backend/SeatifyBackend/Entities/Models/Venue.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.Models
+{
+    public class Venue
+    {
+        public string Id { get; set; }
+        string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public Venue()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
