b532178b9d5a4d71ef9bc659e00b3b27e8383543
NagyBendeguz <bendeguz@medea.hu>
2026-03-18 20:10:07 +0100

    Implement VenueService

---
+++ Backend/SeatifyBackend/Logic/Services/VenueService.cs
@@ -0,0 +1,82 @@
+using Data;
+using Entities.Dtos.Venue;
+using Entities.Models;
+using Logic.Helper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logic.Services;
+
+public class VenueService
+{
+    private readonly Repository<Venue> _venueRepository;
+    DtoProvider dtoProvider;
+
+    public VenueService(Repository<Venue> venueRepository, DtoProvider dtoProvider)
+    {
+        _venueRepository = venueRepository;
+        this.dtoProvider = dtoProvider;
+    }
+    
+    public async Task<Venue> CreateVenueAsync(VenueCreateDto createDto)
+    {
+        var newVenue = dtoProvider.Mapper.Map<Venue>(createDto);
+        _venueRepository.Add(newVenue);
+        return newVenue;
+    }
+
+    public async Task<VenueViewDto> GetVenueByIdAsync(string venueId)
+    {
+        Venue? venue = await _venueRepository.GetAll().FirstOrDefaultAsync(e => e.Id == venueId);
+
+        if (venue == null)
+        {
+            throw new Exception("Venue does not exist!");
+        }
+        
+        VenueViewDto venueViewDto = dtoProvider.Mapper.Map<VenueViewDto>(venue);
+        return venueViewDto;
+    }
+    
+    public async Task<List<VenueViewDto>> GetAllVenuesAsync()
+    {
+        List<VenueViewDto> venues = dtoProvider.Mapper.Map<List<VenueViewDto>>(await _venueRepository.GetAll().ToListAsync());
+        return venues;
+    }
+
+    public async Task<Venue> UpdateVenueByIdAsync(VenueUpdateDto updateDto, string venueId)
+    {
+        Venue? existingVenue = await _venueRepository.GetAll().FirstOrDefaultAsync(e => e.Id == venueId);
+
+        if (existingVenue == null)
+        {
+            throw new Exception("Venue does not exist!");
+        }
+        
+        /*if (existingVenue.OrganizerId != updateDto.OrganizerId)
+        {
+            throw new Exception("The venue does not belong to the logged-in user!");
+        }*/
+
+        var updatedVenue = dtoProvider.Mapper.Map(updateDto, existingVenue);
+        _venueRepository.Update(updatedVenue);
+        return updatedVenue;
+    }
+
+    public async Task<bool> DeleteVenueByIdAsync(string organizerId, string venueId)
+    {
+        Venue? existingVenue = await _venueRepository.GetAll().FirstOrDefaultAsync(e => e.Id == venueId);
+
+        if (existingVenue == null)
+        {
+            throw new Exception("Venue does not exist!");
+        }
+        
+        /*if (existingVenue.OrganizerId != organizerId)
+        {
+            throw new Exception("The venue does not belong to the logged-in user!");
+        }*/
+        
+        _venueRepository.Delete(existingVenue);
+        return true;
+    }
+}
