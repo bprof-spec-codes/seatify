99481e4d7d1bc7d39e00f2abbc697ee09632e999
NagyBendeguz <bendeguz@medea.hu>
2026-03-19 19:02:09 +0100

    Connect Venue to basic Auditorium

--- Backend/SeatifyBackend/Entities/Dtos/Venue/VenueViewDto.cs
+++ Backend/SeatifyBackend/Entities/Dtos/Venue/VenueViewDto.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using Entities.Dtos.Auditorium;
+using Entities.Models;
 
 namespace Entities.Dtos.Venue;
 
@@ -6,14 +8,18 @@ public class VenueViewDto
 {
     [Required]
     public string Id { get; set; } = string.Empty;
+    
     [Required]
     public string Name { get; set; } = string.Empty;
+    
     [Required]
     public string City { get; set; } = string.Empty;
+    
     [Required]
     public string PostalCode { get; set; } = string.Empty;
+    
     [Required]
     public string AddressLine { get; set; } = string.Empty;
     
-    // TODO: a helyszínhez tartozó nézőterek
+    public ICollection<AuditoriumViewDto> Auditoriums { get; set; } = new List<AuditoriumViewDto>();
 }
--- Backend/SeatifyBackend/Entities/Models/Venue.cs
+++ Backend/SeatifyBackend/Entities/Models/Venue.cs
@@ -6,16 +6,19 @@ namespace Entities.Models
     public class Venue : IIdEntity
     {
         [Key]
-        public string Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string City { get; set; } = string.Empty;
-        public string PostalCode { get; set; } = string.Empty;
-        public string AddressLine { get; set; } = string.Empty;
-        public string OrganizerId { get; set; } = string.Empty; // TODO: FK
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         
-        public Venue()
-        {
-            Id = Guid.NewGuid().ToString();
-        }
+        public string Name { get; set; } = string.Empty;
+        
+        public string City { get; set; } = string.Empty;
+        
+        public string PostalCode { get; set; } = string.Empty;
+        
+        public string AddressLine { get; set; } = string.Empty;
+        
+        // TODO: FK
+        public string OrganizerId { get; set; } = string.Empty;
+        
+        public ICollection<Auditorium> Auditoriums { get; set; } = new List<Auditorium>();
     }
 }
