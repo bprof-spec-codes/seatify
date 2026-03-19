dbb534ce53234566f98008e03f4705670ae9939d
fbarnabas55 <fenyvesi.barnabas55@stud.uni-obuda.hu>
2026-03-19 19:36:46 +0100

    Remove WelcomePage component and add LandingPage component with associated files

--- Frontend/SeatifyFrontend/src/app/app.module.ts
+++ Frontend/SeatifyFrontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './Components/WelcomePage/welcome-page/welcome-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
--- Frontend/SeatifyFrontend/src/app/pages/landing-page/landing-page.component.spec.ts
+++ Frontend/SeatifyFrontend/src/app/pages/landing-page/landing-page.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WelcomePageComponent } from './welcome-page.component';
+import { LandingPageComponent } from './landing-page.component';
 
-describe('WelcomePageComponent', () => {
-  let component: WelcomePageComponent;
-  let fixture: ComponentFixture<WelcomePageComponent>;
+describe('LandingPageComponent', () => {
+  let component: LandingPageComponent;
+  let fixture: ComponentFixture<LandingPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [WelcomePageComponent]
+      declarations: [LandingPageComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(WelcomePageComponent);
+    fixture = TestBed.createComponent(LandingPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
