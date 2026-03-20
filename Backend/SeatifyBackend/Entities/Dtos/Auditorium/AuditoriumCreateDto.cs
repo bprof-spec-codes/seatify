24c18307303b44db1d02a7e88b778af69f8962fc
fbarnabas55 <fenyvesi.barnabas55@gmail.com>
2026-03-20 19:31:13 +0100

    Revert "Feature/seatmap fix"

--- Backend/SeatifyBackend/Entities/Dtos/Auditorium/AuditoriumCreateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Entities.Dtos.Auditorium
-{
-    public class AuditoriumCreateDto
-    {
-        public string VenueId { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; } = string.Empty;
-    }
-}
