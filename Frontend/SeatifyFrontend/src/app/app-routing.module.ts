4594216104fc58fe369158b311890bc162a6d9bc
NagyBendeguz <bendeguz@medea.hu>
2026-03-22 17:36:40 +0100

    Add Venue Dashboard to routes

--- Frontend/SeatifyFrontend/src/app/app-routing.module.ts
+++ Frontend/SeatifyFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { VenueDashboardComponent } from './pages/venue-dashboard/venue-dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  {path: 'landingpage', component: LandingPageComponent},
+  { path: 'landingpage', component: LandingPageComponent },
+  { path: 'venue-dashboard', component: VenueDashboardComponent },
   { path: '**', redirectTo: '' }
 ];
 
