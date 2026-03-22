6847c1cc01df4978aa6905af14e249c3961ab355
fbarnabas55 <fenyvesi.barnabas55@gmail.com>
2026-03-22 15:02:38 +0100

    Add Organizer entity and related DTOs with validation
    Added Organizer model with properties and data annotations. Introduced OrganizerCreateDto, OrganizerUpdateDto, and OrganizerViewDto for data transfer and validation. Included GUID generation for Organizer Id.

---
+++ Backend/SeatifyBackend/Entities/Dtos/Organizer/OrganizerCreateDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos.Organizer
+{
+    public class OrganizerCreateDto
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(30)]
+        public string? BrandColor { get; set; }
+    }
+}
