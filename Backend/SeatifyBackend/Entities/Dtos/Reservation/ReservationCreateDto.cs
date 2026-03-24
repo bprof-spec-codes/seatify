010bbad2079ce5a71fd98109c2d368df82ba4241
Misih26 <homonnai001@stud.uni-obuda.hu>
2026-03-24 18:06:03 +0100

    add ReservationCreateDto

---
+++ Backend/SeatifyBackend/Entities/Dtos/Reservation/ReservationCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos.Reservation
+{
+    public class ReservationCreateDto
+    {
+        public string CustomerName { get; set; } = string.Empty;
+        public string CustomerEmail { get; set; } = string.Empty;
+        public string CustomerPhone { get; set; } = string.Empty;
+        public string EventOccurenceId { get; set; } = string.Empty;
+
+        public List<string> SeatIds { get; set; } = new();
+
+        // TODO: later BookingSessionId
+    }
+}
