debd1f52a5c8834d85371f36b53a3bfb099f28ec
Misih26 <homonnai001@stud.uni-obuda.hu>
2026-03-24 18:10:51 +0100

    define IEventOccurrenceService interface

---
+++ Backend/SeatifyBackend/Logic/Interfaces/IEventOccurrenceService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Dtos.EventOccurrence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Interfaces
+{
+    public interface IEventOccurrenceService
+    {
+        public bool Create(EventOccurrenceCreateDto createDto);
+        public EventOccurrenceViewDto? GetById(string id);
+        public bool Update(string id, EventOccurrenceCreateDto updateDto);
+        public bool Delete(string id);
+        public object GetReservations(string id);
+    }
+}
