420386cd628d91635ac5f1a581f39f201c0f8ec8
Misih26 <homonnai001@stud.uni-obuda.hu>
2026-03-24 18:00:38 +0100

    add ReservationSeat model

---
+++ Backend/SeatifyBackend/Entities/Models/ReservationSeat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class ReservationSeat
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string ReservationId { get; set; } = string.Empty;
+        public string SeatId { get; set; } = string.Empty;
+        public decimal FinalPrice { get; set; }
+        public virtual Reservation Reservation { get; set; } = null!;
+
+        // TODO: public virtual Seat Seat { get; set; } = null!; 
+    }
+}
+
