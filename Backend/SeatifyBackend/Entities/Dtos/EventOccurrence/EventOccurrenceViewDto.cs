920e70181e58256ad477d066460d9fc8c2f68ec1
Misih26 <homonnai001@stud.uni-obuda.hu>
2026-03-24 18:05:31 +0100

    add EventOccurrenceViewDto

---
+++ Backend/SeatifyBackend/Entities/Dtos/EventOccurrence/EventOccurrenceViewDto.cs
@@ -0,0 +1,51 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos.EventOccurrence
+{
+    public class EventOccurrenceViewDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string EventId { get; set; } = string.Empty;
+        public string VenueId { get; set; } = string.Empty;
+        public string AuditoriumId { get; set; } = string.Empty;
+
+        public DateTime StartsAtUtc { get; set; }
+        public DateTime EndsAtUtc { get; set; }
+        public DateTime BookingOpenAtUtc { get; set; }
+        public DateTime BookingCloseAtUtc { get; set; }
+        public string Status { get; set; } = string.Empty;
+
+        // TODO: change to EventDto
+        public EventViewDto Event { get; set; } = null!;
+
+        // TODO: change to VenueDto
+        public VenueViewDto Venue { get; set; } = null!;
+
+        // TODO: change to AuditoriumDto
+        public AuditoriumViewDto Auditorium { get; set; } = null!;
+    }
+
+    public class EventViewDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+
+    public class VenueViewDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class AuditoriumViewDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
