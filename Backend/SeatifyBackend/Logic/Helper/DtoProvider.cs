c8f0c9b987699c2d3c7abde6a070c388f6af8932
batoriandras <batori.andras@gmail.com>
2026-03-24 22:59:21 +0100

    Add AutoMapper configs for LayoutMatrix and Seat DTOs
    Added mapping profiles in DtoProvider for LayoutMatrix and Seat entities and their related DTOs. This includes create/update mappings with selective property ignores, and view mappings for both LayoutMatrix and Seat, enabling streamlined entity-DTO transformations.

--- Backend/SeatifyBackend/Logic/Helper/DtoProvider.cs
+++ Backend/SeatifyBackend/Logic/Helper/DtoProvider.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using Entities.Dtos.Auditorium;
+using Entities.Dtos.LayoutMatrix;
+using Entities.Dtos.Seat;
 using Entities.Dtos.Venue;
 using Entities.Models;
 using Microsoft.Extensions.Logging;
@@ -9,7 +11,7 @@ namespace Logic.Helper;
 public class DtoProvider
 {
     public Mapper Mapper { get; }
-    
+
     public DtoProvider()
     {
         var config = new MapperConfiguration(cfg =>
@@ -19,6 +21,23 @@ public class DtoProvider
             cfg.CreateMap<Venue, VenueViewDto>()
                 .ForMember(dest => dest.Auditoriums, opt => opt.MapFrom(
                     src => src.Auditoriums.Select(a => new AuditoriumViewDto { Id = a.Id, VenueId = a.VenueId })));
+
+            //LayoutMatrix
+            cfg.CreateMap<LayoutMatrixCreateDto, LayoutMatrix>();
+
+            cfg.CreateMap<LayoutMatrixUpdateDto, LayoutMatrix>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.AuditoriumId, opt => opt.Ignore())
+            .ForMember(dest => dest.Auditorium, opt => opt.Ignore())
+            .ForMember(dest => dest.Seats, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAtUtc, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAtUtc, opt => opt.Ignore());
+
+            cfg.CreateMap<LayoutMatrix, LayoutMatrixViewDto>();
+            cfg.CreateMap<Seat, SeatViewDto>();
+
+            cfg.CreateMap<LayoutMatrix, LayoutMatrixSeatMapDto>()
+                .ForMember(dest => dest.Seats, opt => opt.MapFrom(src => src.Seats));
         }, new LoggerFactory());
         Mapper = new Mapper(config);
     }
