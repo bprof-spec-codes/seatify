2bf1d8a55a889dab8a26ce5de06f8a8e7abec62d
batoriandras <batori.andras@gmail.com>
2026-03-24 23:01:26 +0100

    Refactor: use LayoutMatrixViewDto in Auditorium DTOs
    Replaced LayoutMatrix entity with LayoutMatrixViewDto in AuditoriumViewDto and related service mapping. Updated property types, mapping logic, and using statements to improve DTO/entity separation and API response structure.

--- Backend/SeatifyBackend/Entities/Dtos/Auditorium/AuditoriumViewDto.cs
+++ Backend/SeatifyBackend/Entities/Dtos/Auditorium/AuditoriumViewDto.cs
@@ -1,24 +1,23 @@
-using System.ComponentModel.DataAnnotations;
+using Entities.Dtos.LayoutMatrix;
 using Entities.Dtos.Sector;
-using Entities.Models;
 
 namespace Entities.Dtos.Auditorium;
 
 public class AuditoriumViewDto
 {
     public string Id { get; set; } = string.Empty;
-    
+
     public string VenueId { get; set; } = string.Empty;
-    
+
     public string Name { get; set; } = string.Empty;
-    
+
     public string? Description { get; set; } = string.Empty;
-    
-    public List<LayoutMatrix> LayoutMatrices { get; set; } = new(); //todo: LayoutMatrix helyett LayoutMatrixViewDto-ra cserélni!!
-    
+
+    public List<LayoutMatrixViewDto> LayoutMatrices { get; set; } = new();
+
     public List<SectorViewDto> Sectors { get; set; } = new();
-    
+
     public DateTime CreatedAtUtc { get; set; }
-    
+
     public DateTime UpdatedAtUtc { get; set; }
 }
