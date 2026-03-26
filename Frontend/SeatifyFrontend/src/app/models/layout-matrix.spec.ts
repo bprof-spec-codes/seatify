97fcd9c3d177076c88dd76abe76ef3bdb062bdf1
batoriandras <batori.andras@gmail.com>
2026-03-26 20:28:15 +0100

    feat: Implement LayoutMatrix model and service with corresponding tests

---
+++ Frontend/SeatifyFrontend/src/app/models/layout-matrix.spec.ts
@@ -0,0 +1,7 @@
+import { LayoutMatrix } from './layout-matrix';
+
+describe('LayoutMatrix', () => {
+  it('should create an instance', () => {
+    expect(new LayoutMatrix()).toBeTruthy();
+  });
+});
---
+++ Frontend/SeatifyFrontend/src/app/models/layout-matrix.ts
@@ -0,0 +1,9 @@
+export class LayoutMatrix {
+    id: string = ''
+    auditoriumId: string = ''
+    name: string = ''
+    rows: number = 0
+    columns: number = 0
+    createdAtUtc: Date = new Date()
+    updatedAtUtc: Date = new Date()
+}
