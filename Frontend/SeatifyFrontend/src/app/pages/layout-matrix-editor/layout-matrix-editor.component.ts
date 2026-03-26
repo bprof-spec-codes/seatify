d570b7791481647a932d4dcb652c1b5ac9db407a
batoriandras <batori.andras@gmail.com>
2026-03-26 22:32:10 +0100

    feat: Implement Layout Matrix's getLayoutMatrixByAuditoriumId and Layout Matrix Editor with dynamic matrix selection and grid display

--- Frontend/SeatifyFrontend/src/app/pages/layout-matrix-editor/layout-matrix-editor.component.ts
+++ Frontend/SeatifyFrontend/src/app/pages/layout-matrix-editor/layout-matrix-editor.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutMatrixService } from '../../services/layout-matrix.service';
+import { Observable } from 'rxjs';
+import { LayoutMatrix } from '../../models/layout-matrix';
+import { array } from 'three/src/nodes/core/ArrayNode.js';
 
 @Component({
   selector: 'app-layout-matrix-editor',
@@ -6,6 +10,29 @@ import { Component } from '@angular/core';
   templateUrl: './layout-matrix-editor.component.html',
   styleUrl: './layout-matrix-editor.component.sass'
 })
-export class LayoutMatrixEditorComponent {
+export class LayoutMatrixEditorComponent implements OnInit {
+  matrices$!: Observable<LayoutMatrix[]>
+  selectedMatrix: LayoutMatrix | null = null
+
+  auditoriumId: string = 'aud-id-01' // mock data
+
+  constructor(private matrixService: LayoutMatrixService) { }
+
+  ngOnInit(): void {
+
+    this.matrices$ = this.matrixService.LayoutMatrix$
+
+    this.matrixService.getLayoutMatrixByAuditoriumId(this.auditoriumId).subscribe()
+  }
+
+  selectMatrix(matrix: LayoutMatrix): void {
+    this.selectedMatrix = matrix
+  }
+
+  getGrid(): number[] {
+    if (!this.selectedMatrix) return []
+    return Array.from({ length: this.selectedMatrix.rows * this.selectedMatrix.columns })
+  }
+
 
 }
--- Frontend/SeatifyFrontend/src/app/services/layout-matrix.service.ts
+++ Frontend/SeatifyFrontend/src/app/services/layout-matrix.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { BehaviorSubject, catchError, map, Observable, tap, throwError } from 'rxjs';
+import { LayoutMatrix } from '../models/layout-matrix';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LayoutMatrixService {
+  private apiUrl = `${environment.baseApiUrl}/api`
+  private LayoutMatrixSource = new BehaviorSubject<LayoutMatrix[]>([])
+  LayoutMatrix$ = this.LayoutMatrixSource.asObservable()
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getLayoutMatrixByAuditoriumId(auditoriumId: string): Observable<LayoutMatrix[]> {
+        return this.http.get<LayoutMatrix[]>(`${this.apiUrl}/auditoriums/${auditoriumId}/layout-matrices`).pipe(
+      map(matrices => matrices.map(matrix => this.mapLayoutMatrixDates(matrix))),
+      tap(matrices => this.LayoutMatrixSource.next(matrices)),
+      catchError(this.handleError)
+    );
+  }
+
+  setMatrices(matrices: LayoutMatrix[]): void {
+    this.LayoutMatrixSource.next(matrices)
+  }
+
+  clearMatrices(): void {
+    this.LayoutMatrixSource.next([])
+  }
+
+  private mapLayoutMatrixDates(matrix: LayoutMatrix): LayoutMatrix {
+    return {
+      ...matrix,
+      createdAtUtc: new Date(matrix.createdAtUtc),
+      updatedAtUtc: new Date(matrix.updatedAtUtc)
+    }
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.error('An error occurred: ', error.message)
+    return throwError(() => new Error('Something went wrong; please try again later.'));
+  }
+
 }
