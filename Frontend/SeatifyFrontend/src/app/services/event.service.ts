ae98a890cb573dc8e3a768375d2505e6c7c1ee08
Misih26 <homonnai001@stud.uni-obuda.hu>
2026-03-30 13:46:05 +0200

    create event services

---
+++ Frontend/SeatifyFrontend/src/app/services/event.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import EventRequest from '../models/event.request';
+import { Observable } from 'rxjs';
+import EventResponse from '../models/event.response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  constructor(private http: HttpClient) { }
+
+  createEvent(eventrequest: EventRequest): Observable<EventResponse>{
+    return this.http.post<EventResponse>("http://localhost:4200/create-event", eventrequest);
+  }
+
+  updateEvent(eventrequest: EventRequest, id: number): Observable<EventResponse>{
+    return this.http.put<EventResponse>("http://localhost:4200/update-event/" + id, eventrequest);
+  }
+}
