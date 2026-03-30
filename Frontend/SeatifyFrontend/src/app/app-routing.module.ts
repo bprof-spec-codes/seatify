08fb226b9252833ea82ed08f744ff0f593dd98e9
Misih26 <homonnai001@stud.uni-obuda.hu>
2026-03-30 13:47:47 +0200

    event routing + module

--- Frontend/SeatifyFrontend/src/app/app-routing.module.ts
+++ Frontend/SeatifyFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { CreateEventComponent } from './pages/create-event/create-event.component';
+import { UpdateEventComponent } from './pages/create-event/update-event.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   {path: 'landingpage', component: LandingPageComponent},
+  {path: 'create-event', component:CreateEventComponent},
+  {path: 'update-event', component:UpdateEventComponent},
   { path: '**', redirectTo: '' }
 ];
 
--- Frontend/SeatifyFrontend/src/app/app.module.ts
+++ Frontend/SeatifyFrontend/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { SeatMapDisplayComponent } from './helpers/seat-map-display/seat-map-display.component';
+import { CreateEventComponent } from './pages/create-event/create-event.component';
+import { EventFormComponent } from './pages/create-event/eventform.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,12 +16,14 @@ import { SeatMapDisplayComponent } from './helpers/seat-map-display/seat-map-dis
     LandingPageComponent,
     FooterComponent,
     SeatMapDisplayComponent,
+    //CreateEventComponent,
+    //EventFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
