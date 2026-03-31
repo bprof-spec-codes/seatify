3a2e9a382e8260a3ab65f9f6ae32bc0a7821da6e
fbarnabas55 <fenyvesi.barnabas55@gmail.com>
2026-03-31 16:33:29 +0200

    feat(organizer): add organizer layout and dashboard components with basic structure

---
+++ Frontend/SeatifyFrontend/src/app/pages/organizer-dashboard/organizer-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer-dashboard',
+  standalone: false,
+  templateUrl: './organizer-dashboard.component.html',
+  styleUrl: './organizer-dashboard.component.sass'
+})
+export class OrganizerDashboardComponent {
+
+}
