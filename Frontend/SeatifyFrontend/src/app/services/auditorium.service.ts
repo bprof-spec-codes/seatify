3647c208cda7c69b1217b5868de30ac852e2f8d9
szczukabendeguz <szc.bende@stud.uni-obuda.hu>
2026-03-31 21:21:58 +0200

    implement auditorium and venue services with environment configuration

--- Frontend/SeatifyFrontend/src/app/services/auditorium.service.ts
+++ Frontend/SeatifyFrontend/src/app/services/auditorium.service.ts
@@ -13,10 +13,46 @@ export class AuditoriumService {
   private auditoriumsSource$ = new BehaviorSubject<Auditorium[]>([]);
   auditoriums$ = this.auditoriumsSource$.asObservable();
 
+  private editModeSource = new BehaviorSubject<boolean>(false);
+  editMode$ = this.editModeSource.asObservable();
+
+  private editAuditoriumSource = new BehaviorSubject<Auditorium>(new Auditorium());
+  editAuditorium$ = this.editAuditoriumSource.asObservable();
+
   constructor(private http: HttpClient) {}
+
+  getAuditoriumById(auditoriumId: string): Observable<Auditorium> {
+    return this.http.get<Auditorium>(`${this.apiUrl}/${auditoriumId}`).pipe(
+      catchError(this.handleError)
+    );
+  }
 
   getAuditoriumsByVenueId(venueId: string): Observable<Auditorium[]> {
     return this.http.get<Auditorium[]>(`${this.venuesApiUrl}/${venueId}/auditoriums`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  createAuditorium(venueId: string, auditorium: Auditorium): Observable<Auditorium> {
+    return this.http.post<Auditorium>(`${this.venuesApiUrl}/${venueId}/auditoriums`, auditorium).pipe(
+      tap(newAuditorium => {
+        const updatedAuditoriums = [...this.auditoriumsSource$.getValue(), newAuditorium];
+        this.auditoriumsSource$.next(updatedAuditoriums);
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  updateAuditorium(auditorium: Auditorium): Observable<Auditorium> {
+    return this.http.put<Auditorium>(`${this.apiUrl}/${auditorium.id}`, auditorium).pipe(
+      tap(updatedAuditorium => {
+        const auditoriums = this.auditoriumsSource$.getValue();
+        const index = auditoriums.findIndex(a => a.id === updatedAuditorium.id);
+        if (index !== -1) {
+          auditoriums[index] = { ...auditoriums[index], ...updatedAuditorium };
+          this.auditoriumsSource$.next([...auditoriums]);
+        }
+      }),
       catchError(this.handleError)
     );
   }
@@ -32,8 +68,27 @@ export class AuditoriumService {
     );
   }
 
+  getEditMode(): Observable<boolean> {
+    return this.editMode$;
+  }
+
+  setEditMode(editMode: boolean): void {
+    this.editModeSource.next(editMode);
+  }
+
+  getEditAuditorium(): Observable<Auditorium> {
+    return this.editAuditorium$;
+  }
+
+  setEditAuditorium(editAuditorium: Auditorium): void {
+    this.editAuditoriumSource.next(editAuditorium);
+  }
+
   private handleError(error: HttpErrorResponse): Observable<never> {
     console.error('An error occurred: ', error.message);
+    if (error.error) {
+        console.error('Detailed error from backend: ', error.error);
+    }
     return throwError(() => new Error('Something went wrong; please try again later.'));
   }
 }
