69e0dabbe6bc2d46a8e5d12d28b459db31e3092e
Misih26 <homonnai001@stud.uni-obuda.hu>
2026-04-16 14:59:18 +0200

    Created eventNotFound exception

---
+++ Backend/SeatifyBackend/Entities/Dtos/Exceptions/EventNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos.Exceptions
+{
+    public class EventNotFoundException: Exception
+    {
+        public EventNotFoundException(string msg): base(msg)
+        {
+            
+        }
+    }
+}
