91ed089e58d738125e40d94841bac093789ea840
NagyBendeguz <bendeguz@medea.hu>
2026-05-02 13:05:42 +0200

    Fix namespace

--- Backend/SeatifyBackend/Logic/Services/QrService.cs
+++ Backend/SeatifyBackend/Logic/Services/QrService.cs
@@ -1,30 +1,31 @@
 using QRCoder;
 
-namespace Logic.Services;
-
-public class QrService
+namespace Logic.Services
 {
-    const string QR_CODE_PREFIX = "Reservation:";
-    const string TICKET_QR_CODE_PREFIX = "Ticket:";
-
-    public string GenerateReservationQrCode(string reservationId)
+    public class QrService
     {
-        return GenerateQrCodeBase64(QR_CODE_PREFIX + reservationId);
-    }
+        const string QR_CODE_PREFIX = "Reservation:";
+        const string TICKET_QR_CODE_PREFIX = "Ticket:";
 
-    public string GenerateTicketQrCode(string reservationSeatId)
-    {
-        return GenerateQrCodeBase64(TICKET_QR_CODE_PREFIX + reservationSeatId);
-    }
+        public string GenerateReservationQrCode(string reservationId)
+        {
+            return GenerateQrCodeBase64(QR_CODE_PREFIX + reservationId);
+        }
 
-    public string GenerateQrCodeBase64(string text)
-    {
-        using var qrGenerator = new QRCodeGenerator();
-        using var qrData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
-        var pngQr = new PngByteQRCode(qrData);
-        byte[] qrBytes = pngQr.GetGraphic(20);
+        public string GenerateTicketQrCode(string reservationSeatId)
+        {
+            return GenerateQrCodeBase64(TICKET_QR_CODE_PREFIX + reservationSeatId);
+        }
 
-        // A byte tömb átalakítása Base64 stringgé
-        return Convert.ToBase64String(qrBytes);
+        public string GenerateQrCodeBase64(string text)
+        {
+            using var qrGenerator = new QRCodeGenerator();
+            using var qrData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            var pngQr = new PngByteQRCode(qrData);
+            byte[] qrBytes = pngQr.GetGraphic(20);
+
+            // A byte tömb átalakítása Base64 stringgé
+            return Convert.ToBase64String(qrBytes);
+        }
     }
 }
