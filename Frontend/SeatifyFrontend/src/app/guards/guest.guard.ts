a82c3fe2da3820915987a476b1f2fdd416009703
batoriandras <batori.andras@gmail.com>
2026-05-08 20:11:33 +0200

    Add guest guard implementation

---
+++ Frontend/SeatifyFrontend/src/app/guards/guest.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const guestGuard: CanActivateFn = (): boolean | UrlTree => {
+  const authService = inject(AuthService)
+  const router = inject(Router)
+
+  if (!authService.isLoggedIn()) {
+    return true
+  }
+
+  return router.createUrlTree(['/dashboard'])
+};
