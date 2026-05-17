2953f2750e7ebf3990160ad7de4419e6ebea1c5f
szczukabendeguz <szc.bende@stud.uni-obuda.hu>
2026-05-17 20:49:11 +0200

    fix: correct import statements in landing-page component

--- Frontend/SeatifyFrontend/src/app/pages/landing-page/landing-page.component.ts
+++ Frontend/SeatifyFrontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router} from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { SeatMapDisplayComponent } from '../../helpers/seat-map-display/seat-map-display.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common';}
 
 @Component({
   selector: 'app-landing-page',
